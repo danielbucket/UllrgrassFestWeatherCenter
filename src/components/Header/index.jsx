0c5ff91a43ece4eb7de82c9d512d0c802b772938
Daniel Bucket <danieljbucket@gmail.com>
2025-01-22 10:12:02 -0700

    make the banner/header clickable to redirect client to ullrgass.com

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,9 +4,16 @@ import ullrLogo from '../../assets/ullrlogohorizontalyellow.png';
 export default function Header() {
   const year = new Date().getFullYear().toString().split('');
 
+  const handleClick = () => {
+    window.location.href = 'https://ullrgrass.com';
+  }
+
   return (
     <div className="header">
-      <img src={ullrLogo} alt="Ullr Logo" />
+      <img
+        src={ullrLogo}
+        alt="Ullr Logo"
+        onClick={() => handleClick()} />
         <p className="year">
           {
             year.map((num, i) => <span key={i}>{num}</span>)
